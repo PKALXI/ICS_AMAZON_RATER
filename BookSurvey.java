6a68b943eb05750b53582870011c444725e2f71c
LukeCihra1 <72084254+LukeCihra1@users.noreply.github.com>
2020-10-08 14:56:10 -0400

    Update BookSurvey.java

--- BookSurvey.java
+++ BookSurvey.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package mysurvey;
+//package mysurvey;
 
 /**
  * @purpose This page obtains books, genres and authors the user likes to find which books are best suited to their tastes
