c6de6e06889fe0fe3b1eea96eeac9f796d38610d
LukeCihra1 <72084254+LukeCihra1@users.noreply.github.com>
2020-10-08 14:55:35 -0400

    Update BookSurvey.java

--- BookSurvey.java
+++ BookSurvey.java
@@ -1,3 +1,10 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mysurvey;
+
 /**
  * @purpose This page obtains books, genres and authors the user likes to find which books are best suited to their tastes
  * @author Luke
